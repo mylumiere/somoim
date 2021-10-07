2c421a59ce69c75764f5fefceff4cd78c41ed957
mylumiere <matues00@naver.com>
2021-10-08 04:16:40 +0900

    complete_calendar_date

--- front/src/app/main/main.component.ts
+++ front/src/app/main/main.component.ts
@@ -20,9 +20,10 @@ export class MainComponent implements OnInit {
   selectedDate: string;
   signedInUser: User;
 
+
+
   constructor(
     private articleService: ArticleService,
-    
     private userService: UserService,
     private router: Router,
   ) { }
@@ -50,4 +51,38 @@ export class MainComponent implements OnInit {
     this.signedInUser = null;
     this.router.navigate(['/sign_in'])
   }
+
+  // For calendar
+  days = ['일','월','화','수','목','금','토'];
+  arrayDays = [...Array(7).keys()];
+
+  today = new Date();
+  year = this.today.getFullYear();
+  month = this.today.getMonth()+1;
+  numDates = new Date(this.year,this.month,0).getDate();
+  firstDay = new Date(this.year,this.month-1,1).getDay();
+  firstSunday = 1-this.firstDay;
+  numWeeks = Math.ceil((this.numDates-this.firstSunday)/7)+1
+  arrayWeeks = [...Array(this.numWeeks).keys()];
+
+  toLastMonth(): void {
+    let lastMonth = new Date(this.year, this.month-2);
+    this.moveMonth(lastMonth);
+  }
+
+  toNextMonth(): void {
+    let nextMonth = new Date(this.year, this.month);
+    this.moveMonth(nextMonth);
+  }
+
+  moveMonth(month): void {
+    this.year = month.getFullYear();
+    this.month = month.getMonth()+1;
+    this.numDates = new Date(this.year,this.month,0).getDate();
+    this.firstDay = new Date(this.year,this.month-1,1).getDay();
+    this.firstSunday = 1-this.firstDay;
+    this.numWeeks = Math.floor((this.numDates-this.firstSunday)/7)+1;
+    this.arrayWeeks = [...Array(this.numWeeks).keys()];
+  }
+
 }
